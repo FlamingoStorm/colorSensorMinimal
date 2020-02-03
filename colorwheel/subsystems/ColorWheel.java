5998fbe724fad23899a56553ef67f311cbf81c28
FlamingoStorm <43747812+FlamingoStorm@users.noreply.github.com>
2020-02-02 20:53:57 -0500

    Update ColorWheel.java

--- colorwheel/subsystems/ColorWheel.java
+++ colorwheel/subsystems/ColorWheel.java
@@ -11,10 +11,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.I2C;
 
@@ -22,23 +26,16 @@ public class ColorWheel extends SubsystemBase {
   /**
    * Creates a new ColorWheel.
    */
-  public char colorCase;
+  
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort); 
     private final ColorMatch colorMatcher = new ColorMatch();
-    public static final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    public static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    public static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    public static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
     public Color detectedColor = colorSensor.getColor();
     public final  ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
     public String colorString;
     public final int proximity = colorSensor.getProximity();
   public ColorWheel() {
-  colorMatcher.addColorMatch(kBlueTarget);
-  colorMatcher.addColorMatch(kGreenTarget);
-  colorMatcher.addColorMatch(kRedTarget);
-  colorMatcher.addColorMatch(kYellowTarget);
+ 
   }
 
   @Override
@@ -47,21 +44,7 @@ public class ColorWheel extends SubsystemBase {
    
          
          
-         if (match.color == kBlueTarget){
-             colorString = "Blue";
-         }
-         else if (match.color == kRedTarget){
-             colorString = "Red";
-         }
-         else if (match.color == kGreenTarget){
-             colorString = "Green";
-         }
-         else if (match.color == kYellowTarget){
-             colorString = "Yellow";
-         }
-         else{
-             colorString = "Unknown";
-         }
+       
           final double IR = colorSensor.getIR();
           
           SmartDashboard.putNumber("Red", detectedColor.red );
@@ -72,36 +55,7 @@ public class ColorWheel extends SubsystemBase {
           SmartDashboard.putNumber("Proximity", proximity);  
           SmartDashboard.putNumber("Confidence", match.confidence);
           SmartDashboard.putString("Detected Color", colorString);
-          String gameData;
-  gameData = DriverStation.getInstance().getGameSpecificMessage();
-  if(gameData.length() > 0)
-  {
-    switch (gameData.charAt(0))
-    {
-      case 'B' :
-       Robot.colorCase = kRedTarget;
-       
-       //Blue case code
-        break;
-      case 'G' :
-       Robot.colorCase = kYellowTarget;
-       
-        //Green case code
-        break;
-      case 'R' :
-          Robot.colorCase = kBlueTarget;
           
-        //Red case code
-        break;
-      case 'Y' :
-          Robot.colorCase = kGreenTarget;
-          
-        //Yellow case code
-        break;
-      default :
-        //This is corrupt data
-        break;
-          }
       
       }
     
